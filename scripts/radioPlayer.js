f9cc81dadfcb1869f67c8a0eb11a59d13d9e0cba
danko.anton <danko.anton@gmail.com>
2020-08-03 12:57:02 +0300

    added some radiostation

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -43,6 +43,7 @@ export const radioPlayerInit = () => {
 		try {
 			audio.play();
 			changeIdonPlay();
+			console.log('audio.src :>> ', audio.src);
 		} catch (err) {
 			console.error(err);
 			window.location.href = 'audio.src';
